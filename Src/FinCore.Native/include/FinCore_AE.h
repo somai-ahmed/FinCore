10461b41742d7912a963be90bfc69cb6b4d19db3
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-08-22 12:19:24 +0100

    Update FinCore_AE.h

--- Src/FinCore.Native/include/FinCore_AE.h
+++ Src/FinCore.Native/include/FinCore_AE.h
@@ -1,1 +1,12 @@
-
+/*
+ * FinCore Accounting Engine
+ * Data Contract Header - FinCore_AE.h
+ *
+ * Pure C data structures, enums, and constants.
+ * No function declarations. No C++ guards. No export macros.
+ *
+ * Used by:
+ *   - C core engine implementation
+ *   - VB.NET P/Invoke marshaling layer
+ *   - Documentation / interop contract
+ */
