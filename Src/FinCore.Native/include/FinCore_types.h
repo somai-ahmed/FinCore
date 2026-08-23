b90f7dc881f592b3e2217315363b208a00546042
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-08-23 14:31:05 +0100

    Add FinCore_classes_comptabilite enumeration
    Added an enumeration for accounting classes in FinCore_types.h.

--- Src/FinCore.Native/include/FinCore_types.h
+++ Src/FinCore.Native/include/FinCore_types.h
@@ -28,3 +28,13 @@ typedef uint32_t FinCore_LineId;
 /* ------------------------------------------------------------
  * Classification des comptes (plan comptable de type PCG tunisien / français)
  * ------------------------------------------------------------ */
+typedef enum FinCore_classes_comptabilite {
+    FinCore_CLASS_1_CP&PNC = 1,   /* Comptes de capitaux propres et passifs non courants */
+    FinCore_CLASS_2_ANC = 2,
+    FinCore_CLASS_3_STOCKS = 3,
+    FinCore_CLASS_4_TIERS = 4,
+    FinCore_CLASS_5_FINANCIER = 5,
+    /*CHARGES ET PRODUITS ne sont pas des classes mais on le met ici pour le coder*/
+    FinCore_CLASS_6_CHARGES = 6,
+    FinCore_CLASS_7_PRODUITS = 7
+} FinCore_classes_comptabilite ;
