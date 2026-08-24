1d7cff4de8fa8591cca20415e8507a3816feef73
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-08-24 10:31:14 +0100

    Rename FinCore types to FNC for consistency

--- Src/FinCore.Native/include/FinCore_types.h
+++ Src/FinCore.Native/include/FinCore_types.h
@@ -11,17 +11,17 @@
 
 /* Représentation monétaire en virgule fixe : unités entières mineures (ex. millimes)
  * pour éviter les erreurs d'arrondi en virgule flottante dans les calculs comptables. */
-typedef int64_t FinCore_Monnaie;
+typedef int64_t FNC_Monnaie;
 
-#define FinCore_Monnaie_Unite 1000  /* 3 decimal places (millimes) */
+#define FNC_Monnaie_Unite 1000  /* 3 decimal places (millimes) */
 
 /*structure interne masquée aux consommateurs */
-typedef struct FinCore_Session FinCore_Session;
+typedef struct FNC_Session FNC_Session;
 
 /* Identifiants uniques */
 typedef uint32_t id_compte;
-typedef uint32_t FinCore_JournalEntryId;
-typedef uint32_t FinCore_PeriodId;
+typedef uint32_t FNC_JournalEntryId;
+typedef uint32_t FNC_PeriodId;
 typedef uint32_t id_ligne;
 
 #define INVALID_ID 0
@@ -30,41 +30,41 @@ typedef uint32_t id_ligne;
  * Classification des comptes (plan comptable de type PCG tunisien / français)
  * ------------------------------------------------------------ */
 /*see documentation/classes_comptabilite.md*/
-typedef enum FinCore_ClasseCompte {
-    FinCore_CLASSE_1_CP_ET_PNC = 1,   /* Comptes de capitaux propres et passifs non courants */
-    FinCore_CLASSE_2_ACTIFS_NON_COURANTS = 2,
-    FinCore_CLASSE_3_STOCKS = 3,
-    FinCore_CLASSE_4_TIERS = 4,
-    FinCore_CLASSE_5_TRESORERIE = 5,
+typedef enum FNC_ClasseCompte {
+    FNC_CLASSE_1_CP_ET_PNC = 1,   /* Comptes de capitaux propres et passifs non courants */
+    FNC_CLASSE_2_ACTIFS_NON_COURANTS = 2,
+    FNC_CLASSE_3_STOCKS = 3,
+    FNC_CLASSE_4_TIERS = 4,
+    FNC_CLASSE_5_TRESORERIE = 5,
     /* Les charges et produits ne sont pas des classes au sens strict,
        mais on les code ici pour l'uniformité du modèle. */
-    FinCore_CLASSE_6_CHARGES = 6,
-    FinCore_CLASSE_7_PRODUITS = 7
-} FinCore_ClasseCompte;
+    FNC_CLASSE_6_CHARGES = 6,
+    FNC_CLASSE_7_PRODUITS = 7
+} FNC_ClasseCompte;
 
-typedef enum FinCore_TypeCompte {
-    FinCore_COMPTE_ACTIF = 1,          
-    FinCore_COMPTE_PASSIF = 2,    
-    FinCore_COMPTE_CAPITAUX = 3,    
-    FinCore_COMPTE_PRODUIT = 4,   
-    FinCore_COMPTE_CHARGE = 5    
-} FinCore_TypeCompte;
+typedef enum TypeCompte {
+    FNC_COMPTE_ACTIF = 1,          
+    FNC_COMPTE_PASSIF = 2,    
+    FNC_COMPTE_CAPITAUX = 3,    
+    FNC_COMPTE_PRODUIT = 4,   
+    FNC_COMPTE_CHARGE = 5    
+} FNC_TypeCompte;
 
-typedef enum FinCore_SoldeNormal {
-    FinCore_SOLDE_DEBITEUR = 0,        /* DEBIT */
-    FinCore_SOLDE_CREDITEUR = 1        /* CREDIT */
-} FinCore_SoldeNormal;
+typedef enum FNC_SoldeNormal {
+    FNC_SOLDE_DEBITEUR = 0,        /* DEBIT */
+    FNC_SOLDE_CREDITEUR = 1        /* CREDIT */
+} FNC_SoldeNormal;
 
-typedef struct FinCore_Compte {
-    FinCore_IdCompte     id;
+typedef struct Compte {
+    FNC_IdCompte     id;
     char                 code[16];           /* ex. "512000" */
     char                 nom[128];
-    FinCore_ClasseCompte classe;
-    FinCore_TypeCompte   type;
-    FinCore_SoldeNormal  solde_normal;
-    FinCore_IdCompte     parent_id;          /* 0 si racine */
+    FNC_ClasseCompte classe;
+    FNC_TypeCompte   type;
+    FNC_SoldeNormal  solde_normal;
+    FNC_IdCompte     parent_id;          /* 0 si racine */
     int                  est_active;
-} FinCore_Compte;
+} FNC_Compte;
 
 /*--------------------------------------------------------------
     l implementation des structures pour la journal
@@ -72,7 +72,8 @@ typedef struct FinCore_Compte {
 typedef struct ligne_journal{
     id_ligne id;
     id_compte compte_id;
-    FinCore_Monnaie Debit;
-    FinCore_Monnaie credit ;
+    FNC_Monnaie Debit;
+    FNC_Monnaie credit ;
     char libelle[256] ;
 }ligne_journal;
+
