02bb788544a2335a70b97f7167ccab8e8cf973cc
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-08-27 10:42:47 +0100

    Update FinCore_detection.h

--- Src/FinCore.Native/include/FinCore_detection.h
+++ Src/FinCore.Native/include/FinCore_detection.h
@@ -4,3 +4,9 @@
 #include "FinCore_types.h"
 #include "FinCore_errors.h"
 
+typedef enum FinCore_DetectionMethod {
+    FinCore_DETECT_BENFORD = 0,
+    FinCore_DETECT_DOUBLONS = 1,
+    FinCore_DETECT_NOMBRES_RONDS = 2,
+    FinCore_DETECT_VALS_ABERRANTES = 3
+} FinCore_DetectionMethod;
