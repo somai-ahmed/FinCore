1de6ddd4656858cc07d62bfb27bc1dbb5b159eca
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-09-03 10:29:55 +0100

    Add index.c for Utils source implementation
    Created index.c to implement header files in Utils/include.

---
+++ Src/FinCore.Native/Src/Utils/Src/index.c
@@ -0,0 +1,3 @@
+/*la creatio du fichier index.c pour le permettre du creer le sourssier UTILS/src qui va implementer le 
+fichier "headers" .h , situe en Utils/include :: ce fichier est supplementatire :: le moment ou
+  on accelere le travail elle va automatiquement et naturellement supprimee*/
