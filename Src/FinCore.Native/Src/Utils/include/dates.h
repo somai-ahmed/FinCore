5f47935f4181b5b0b7289c5737126dceebfa67c7
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-09-03 09:53:07 +0100

    Update dates.h

--- Src/FinCore.Native/Src/Utils/include/dates.h
+++ Src/FinCore.Native/Src/Utils/include/dates.h
@@ -26,11 +26,13 @@ typedef enum {
 
 
 /*----------------------------------
-    FONCTIONS DU VERIFICATIONS
+    FONCTIONS DU VERIFICATION
 -----------------------------------*/
 bool date_valide(DATE d);
 bool est_annee_bissextile(int32_t annee);
 int32_t obtenir_nbr_jours_mois(int32_t annee , int32_t mois); /*pour la verification du mois FEVRIER 28 ou 29*/
+
+/*------------------------------------
 
 int comparer_dates(DATE d1, DATE d2); /*si d1>d2 : fonction retourne >0 sinon <0 :: si la meme date on recoit un ZERO 0 */
 bool date_inclus_dans_periode (DATE d , DATE debut_periode , DATE fin_periode);
