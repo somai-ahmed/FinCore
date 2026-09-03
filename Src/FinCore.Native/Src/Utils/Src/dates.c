2b4406347730b494e85e55c29a009661afcd8e13
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-09-03 12:04:57 +0100

    Implement leap year check in date validation
    Added a function to check for leap years and integrated it into the date validation logic.

--- Src/FinCore.Native/Src/Utils/Src/dates.c
+++ Src/FinCore.Native/Src/Utils/Src/dates.c
@@ -1,7 +1,11 @@
 #include <Src/FinCore.Native/Src/Utils/include/dates.h>
 
+
+bool est_annee_bissextile(int32_t annee){
+    return (annee % 4 == 0 && annee % 100 != 0) || (annee % 400 == 0);
+}
+
 bool date_valide(DATE d){
-    if(!d) return FALSE;
     if (d.jour < 1 || d.mois < 1 || d.mois > 12) return false;
 
     /* mois à 31 jours */ 
@@ -11,3 +15,15 @@ bool date_valide(DATE d){
     /* mois à 30 jours */
     if (d.jour > 30 && (d.mois == 4 || d.mois == 6 || d.mois == 9 || d.mois == 11))
         return false;
+
+    /*fevrier : le cas particulier*/
+    /*l'apppel du la fonction du l'annee bissextile*/
+    if (est_annee_bissextile(d.annee)) {
+            if (d.jour > FEVRIER_JOURS_BISSEXTILE) return false;
+        }
+    else {
+            if (d.jour > FEVRIER_JOURS_NORMAL) return false;
+        }
+    
+    return true;
+}
