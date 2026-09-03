c230864f6a5b577349ee375d1d298cc69fb90a24
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-09-03 11:47:42 +0100

    Enhance date validation for months with 31 days
    Added validation for days in months with 31 days.

--- Src/FinCore.Native/Src/Utils/Src/dates.c
+++ Src/FinCore.Native/Src/Utils/Src/dates.c
@@ -3,4 +3,7 @@
 bool date_valide(DATE d){
     if(!d) return FALSE;
     if (d.jour < 1 || d.mois < 1 || d.mois > 12) return false;
-    
+    /* mois à 31 jours */ 
+    if (d.jour > 31 && (d.mois == 1 || d.mois == 3 || d.mois == 5 || d.mois == 7 || d.mois == 8 || d.mois == 10 || d.mois == 12))
+        return false;
+
