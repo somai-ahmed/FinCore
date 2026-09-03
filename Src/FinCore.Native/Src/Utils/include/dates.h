325c2a4568a5aa805a6135642ac9a1253f05155c
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-09-03 10:15:58 +0100

    Add date analysis and formatting function declarations

--- Src/FinCore.Native/Src/Utils/include/dates.h
+++ Src/FinCore.Native/Src/Utils/include/dates.h
@@ -52,4 +52,7 @@ DATE jdn_en_date (int32_t jdn);
     elle retourne :
  * "TRUE" en cas de succès, "FALSE" en cas d'échec.
 --------------------------------------------------*/
+bool analyser_date(const char *date_en_texte , DATE_FORMAT format , DATE *resultat);
+bool formater_date(DATE date , DATE_FORMAT format , char *buffer , int32_t taille_buffer)
+
 #endif
