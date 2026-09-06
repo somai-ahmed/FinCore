1103cd54a02d74e51ef8e7e61b9f798c406530a0
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-09-06 16:46:07 +0100

    Update monnaie.c

--- Src/FinCore.Native/Src/Utils/Src/monnaie.c
+++ Src/FinCore.Native/Src/Utils/Src/monnaie.c
@@ -7,3 +7,11 @@ monnaie initiation_monnaie(int8_t echelle){
     m.echelle = echelle;
     return m;
 }
+
+monnaie monnaie_depuis_unites_mineures( int64_t unites_min, int8_t echelle){
+    monnaie m = initiation_monnaie(echelle);
+    m.unites_min = unites_min ;
+    return m;
+}
+
+    
