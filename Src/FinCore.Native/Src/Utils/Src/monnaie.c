0e8a1a64f483a909a78ffc5a619621e589e9d1d2
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-09-06 16:43:24 +0100

    Implement initiation_monnaie function
    Add function to initialize a monetary amount with a specific scale.

--- Src/FinCore.Native/Src/Utils/Src/monnaie.c
+++ Src/FinCore.Native/Src/Utils/Src/monnaie.c
@@ -1,1 +1,9 @@
 #include <Src/FinCore.Native/Src/Utils/include/monnaie.h>
+
+/* creation un montant nul a un echelle specefique */
+monnaie initiation_monnaie(int8_t echelle){
+    monnaie m;
+    m.unites_min  = 0;
+    m.echelle = echelle;
+    return m;
+}
