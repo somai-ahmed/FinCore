8bc894d59825577a5c13d24ec0aa2e9703fa9860
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-09-07 01:39:02 +0100

    Update identifiants.h

--- Src/FinCore.Native/Src/Utils/include/identifiants.h
+++ Src/FinCore.Native/Src/Utils/include/identifiants.h
@@ -5,6 +5,11 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+/* Generateur d'identifiants uniques pour les entites du moteur
+ * (comptes, ecritures, etc.)*/
+typedef struct {
+    int64_t compteur;
+} generateur_id;
 
 
 #endif
