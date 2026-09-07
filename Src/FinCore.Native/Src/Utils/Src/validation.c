f06fd81c485c3e4049c666d7b2b18b9d0745717f
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-09-07 10:45:19 +0100

    Update validation.c

--- Src/FinCore.Native/Src/Utils/Src/validation.c
+++ Src/FinCore.Native/Src/Utils/Src/validation.c
@@ -1,1 +1,9 @@
 #include <Src/FinCore.Native/Src/Utils/include/validation.h>
+
+#include <string.h>
+
+bool valider_chaine_non_vide(const char *chaine){
+  if(chaine==NULL) return false;
+  if(*chaine == '\0') return false;  
+  return true;
+}
