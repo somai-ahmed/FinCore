b83ce782b3f9371fde2934423b4f10e467636685
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-09-07 10:02:54 +0100

    Update identifiants.c

--- Src/FinCore.Native/Src/Utils/Src/identifiants.c
+++ Src/FinCore.Native/Src/Utils/Src/identifiants.c
@@ -12,3 +12,7 @@ int64_t generateur_id_suivant(generateur_id *gen);
     gen->compteur++;
     return gen->compteur;
 }
+
+bool generateur_id_reference(generateur_id *gen, const char *prefixe, int largeur_min, char *tampon, size_t taille_tampon){
+    if (!gen || !prefixe || largeur_min ==0 ||taille_tampon <0) return false ;
+    
