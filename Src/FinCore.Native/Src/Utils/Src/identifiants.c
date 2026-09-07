517040112ce94c043cf9742d92c75485c39ca894
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-09-07 09:58:53 +0100

    Fix pointer dereference for compteur in generateur_id_suivant

--- Src/FinCore.Native/Src/Utils/Src/identifiants.c
+++ Src/FinCore.Native/Src/Utils/Src/identifiants.c
@@ -10,5 +10,5 @@ generateur_id initiation_generateur_id(int64_t valeur_depart){
 int64_t generateur_id_suivant(generateur_id *gen);
     if (!gen) return -1;
     gen->compteur++;
-    return gen.compteur;
+    return gen->compteur;
 }
