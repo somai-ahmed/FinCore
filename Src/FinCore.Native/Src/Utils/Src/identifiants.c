e2f3cb861c9a215d17ba18c086cdc196543f67f7
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-09-07 09:55:57 +0100

    Update identifiants.c

--- Src/FinCore.Native/Src/Utils/Src/identifiants.c
+++ Src/FinCore.Native/Src/Utils/Src/identifiants.c
@@ -1,2 +1,8 @@
 #include <Src/FinCore.Native/Src/Utils/include/identifiants.h>
 #include <string.h>
+
+generateur_id initiation_generateur_id(int64_t valeur_depart){
+    generateur_id gen;
+    gen.compteur = 0;
+    return gen;
+}
