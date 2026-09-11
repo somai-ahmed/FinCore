83d5eebbc1acab729cf51a1c98a5a1612b9bcfd0
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-09-11 11:42:02 +0100

    Update comptes.h

--- Src/FinCore.Native/Src/Utils/include/comptes.h
+++ Src/FinCore.Native/Src/Utils/include/comptes.h
@@ -9,13 +9,16 @@
 #include <identifiants.h>
 
 /* Création et validation */
-FinCore_Etat comptes_creer(Compte *compte, const char *code, const char *nom, FNC_ClasseCompte classe, FNC_TypeCompte type, FNC_SoldeNormal solde_normal, id_compte parent_id);
+Etat comptes_creer(Compte *compte, const char *code, const char *nom, FNC_ClasseCompte classe, FNC_TypeCompte type, FNC_SoldeNormal solde_normal, id_compte parent_id);
 
 bool comptes_valider_code(const char *code);
 bool comptes_valider(const Compte *compte);
 
 /* Opérations sur le solde */
-FinCore_Etat comptes_debiter(Compte *compte, FNC_Monnaie montant);
-FinCore_Etat comptes_crediter(Compte *compte, FNC_Monnaie montant);
-FNC_Monnaie comptes_get_solde(const Compte *compte);
+Etat comptes_debiter(Compte *compte, FNC_Monnaie montant);
+Etat comptes_crediter(Compte *compte, FNC_Monnaie montant);
+Monnaie comptes_avoir_solde(const Compte *compte);
 
+/* Hiérarchie */
+bool comptes_a_parent(const Compte *compte);
+bool comptes_est_enfant_de(const Compte *compte, id_compte parent_id);
