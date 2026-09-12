d86a01679372560617dca4698ad76429d515b23d
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-09-12 02:49:19 +0100

    Update monnaie_convert.c

--- Src/FinCore.Native/Src/Utils/Src/monnaie_convert.c
+++ Src/FinCore.Native/Src/Utils/Src/monnaie_convert.c
@@ -1,1 +1,13 @@
 #include <Src/FinCore.Native/Src/Utils/include/monnaie_convert.h>
+
+#include <stdint.h>
+
+/* ajouter cette fonction sous le caractere static car elle est dedie juste a faciliter le fonctionnement du cette fichier */
+static int64_t puissance_de_dix(int n) {
+    int64_t resultat = 1;
+    int i;
+    for (i = 0; i < n; i++) {
+        resultat *= 10;
+    }
+    return resultat;
+}
