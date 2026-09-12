2ea0e8456f4819fda1ee0ed01e9c62fe9867c573
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-09-12 02:46:04 +0100

    Update monnaie_convert.h

--- Src/FinCore.Native/Src/Utils/include/monnaie_convert.h
+++ Src/FinCore.Native/Src/Utils/include/monnaie_convert.h
@@ -5,4 +5,13 @@
 #include "types.h"
 #include "monnaie.h"
 
+/* Echelle fixe utilisee par le type Monnaie du moteur comptable :
+ * 3 decimales (millimes), coherent avec Monnaie_Unite = 1000. */
+#define MONNAIE_FIXE_ECHELLE 3
+
+/* Convertit une Monnaie (echelle fixe 3) vers une monnaie a echelle
+ * variable. Ne peut pas echouer. */
+monnaie monnaie_depuis_fixe(Monnaie m);
+
+
 #endif
