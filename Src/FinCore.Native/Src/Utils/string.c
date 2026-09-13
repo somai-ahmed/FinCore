ff6b8309ae8626da29779392d0bf5291042fce57
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-09-13 15:14:59 +0100

    Add function to calculate string length

---
+++ Src/FinCore.Native/Src/Utils/string.c
@@ -0,0 +1,12 @@
+#include "string.h"
+#include <stddef.h>
+
+int longeurchaine(const char *chaine){
+  size_t len = 0;
+  if (str) {
+    for (; str[len] != '\0'; len++) {
+    }
+  }
+  return len ;
+}
+
