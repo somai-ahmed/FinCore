b954057f9c3bb77b141131654c5c8e9a1cc8467c
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-09-13 16:02:24 +0100

    Update math_utils.h

--- Src/FinCore.Native/Src/Utils/math_utils.h
+++ Src/FinCore.Native/Src/Utils/math_utils.h
@@ -12,4 +12,10 @@ double math_moyenne(const double *valeurs, size_t nombre);
 double math_ecart_type(const double *valeurs, size_t nombre);
 double math_ecart_absolu_moyen(const double *valeurs, size_t nombre);
 
+/* -------------------------------
+    utilitaires numeriques
+---------------------------------*/
+int64_t math_min_i64(int64_t a, int64_t b);
+int64_t math_max_i64(int64_t a, int64_t b);
+double math_arrondir(double valeur, int decimales);
 #endif
