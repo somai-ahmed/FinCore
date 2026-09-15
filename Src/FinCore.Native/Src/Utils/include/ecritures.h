d2b85f5b0f0f2074f504fc6a0ab3d936c36f81d7
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-09-15 03:21:45 +0100

    Remove ecritures.h header file

---
+++ Src/FinCore.Native/Src/Utils/include/ecritures.h
@@ -0,0 +1,9 @@
+#ifndef ECRITURES_H
+#define ECRITURES_H
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <Src/FinCore.Native/include/types.h>
+#include <Src/FinCore.Native/include/errors.h>
+#include <comptes.h>
