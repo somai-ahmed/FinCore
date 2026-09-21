6e25eae8f8e07a440ddb49f13057a33987ebcead
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-09-21 02:25:21 +0100

    Update grand_livre.c

--- Src/FinCore.Native/Src/Rapports/grand_livre.c
+++ Src/FinCore.Native/Src/Rapports/grand_livre.c
@@ -1,1 +1,6 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "src/FinCore.Native/include/rapports.h"
+#include "src/FinCore.Native/include/ecritures.h"
+#include "src/FinCore.Native/include/periodes.h"
