4c2dc112953bd201ec910120e09118cf8ef01b95
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-09-21 01:23:23 +0100

    Update balance.c

--- Src/FinCore.Native/Src/Rapports/balance.c
+++ Src/FinCore.Native/Src/Rapports/balance.c
@@ -1,1 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+#include "src/FinCore.Native/include/rapports.h"
+#include "src/FinCore.Native/include/ecritures.h"
+#include "src/FinCore.Native/include/periodes.h"
+
 
